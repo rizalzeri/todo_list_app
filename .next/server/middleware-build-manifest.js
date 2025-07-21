e93fb9d82d42dca7920244f15df66a01819f7e29
RIZAL ZERI SUBAKTI <rizal-zeri.subakti@citratubindo.co.id>
2025-07-21 12:04:02 +0700

    configure version next and node

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -10,6 +10,11 @@ globalThis.__BUILD_MANIFEST = {
   "rootMainFiles": [],
   "rootMainFilesTree": {},
   "pages": {
+    "/": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/index.js"
+    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -19,6 +24,11 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
+    ],
+    "/home": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/home.js"
     ],
     "/tasks/task_calendar": [
       "static/chunks/webpack.js",
